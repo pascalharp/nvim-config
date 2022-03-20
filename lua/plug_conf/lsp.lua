1080c967b1ad98d7f1c6ee84e7752f500e0f106b
Pascal H <p.harprecht@gmail.com>
2022-03-20 12:28:41 +0100

    add vuels

--- lua/plug_conf/lsp.lua
+++ lua/plug_conf/lsp.lua
@@ -38,7 +38,7 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 -- servers that have decent autoconf
-local servers = { 'pyright', 'tsserver', 'clangd' }
+local servers = { 'pyright', 'tsserver', 'vuels', 'clangd' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
